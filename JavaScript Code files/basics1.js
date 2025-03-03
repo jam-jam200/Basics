8257c53b556467b18d14910a46fa279c79b6ef2e
jam-jam200 <jessieoyaks4j@gmail.com>
2025-03-03 20:22:40 +0100

    first line

--- JavaScript Code files/basics1.js
+++ JavaScript Code files/basics1.js
@@ -1,6 +1,15 @@
 console.log("Hello World");
 
 let a = 20;
-typeof(a)
+typeof a;
 
 console.log(a, typeof a);
+
+let c = "Bitcoin";
+
+let required = true;
+
+console.log(typeof(a));
+console.log(typeof (c));
+console.log(typeof (required));
+
