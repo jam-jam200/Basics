7360853a0645bf58a5ae5a9b8782367218f7ce8d
jam-jam200 <jessieoyaks4j@gmail.com>
2025-03-03 20:31:56 +0100

    first line

--- JavaScript Code files/basics1.js
+++ JavaScript Code files/basics1.js
@@ -1,6 +1,7 @@
 console.log("Hello World");
 
 let a = 20;
+let b = 12
 typeof a;
 
 console.log(a, typeof a);
@@ -12,4 +13,8 @@ let required = true;
 console.log(typeof(a));
 console.log(typeof (c));
 console.log(typeof (required));
+
+console.log(a+b);
+
+console.log(!required);
 
